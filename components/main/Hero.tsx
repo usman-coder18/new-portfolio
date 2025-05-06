c07ab31bb156dc6f84e9eac158d263329d339882
Usman-coder21 <usmanrafique078@gmail.com>
2025-05-06 18:40:51 +0500

    first commit

---
+++ components/main/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import HeroContent from '../sub/HeroContent'
+
+const Hero = () => {
+  return (
+    <div className='relative flex flex-col h-full w-full '>
+        <video src=""
+        autoPlay
+        muted
+        loop
+        className='absolute top-[-340px] left-0 z-[1] w-full h-full object-cover rotate-180 '>
+            <source src='/blackhole.webm' type='video/webm'/>
+        </video>
+        <HeroContent/>
+
+
+    </div>
+  )
+}
+
+export default Hero
